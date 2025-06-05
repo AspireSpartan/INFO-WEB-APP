b68560768542248d642c09f374ff47b96edf2601
AspireSpartan <134720445+AspireSpartan@users.noreply.github.com>
2025-06-05 18:15:42 +0800

    updates little
    updates little

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -131,8 +131,8 @@
         <div class="flex items-center gap-x-4 mt-auto">
           <img src="https://randomuser.me/api/portraits/women/44.jpg" alt="" class="w-10 h-10 rounded-full bg-gray-50">
           <div>
-            <p class="font-semibold text-gray-900">Lindsay Walton</p>
-            <p class="text-gray-600 text-sm">Front-end Developer</p>
+            <p class="font-semibold text-gray-900">Janpaul Bustillo</p>
+            <p class="text-gray-600 text-sm">Backend Developer</p>
           </div>
         </div>
       </article>
@@ -155,6 +155,72 @@
       </article>
     </div>
   </div>
+
+  <div class="bg-white py-24 sm:py-32">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Our team</h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        We’re a dynamic group of individuals who are passionate about what we do<br>
+        and dedicated to delivering the best results for our clients.
+      </p>
+    </div>
+    <ul role="list" class="mx-auto mt-20 grid max-w-4xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
+      <li class="flex flex-col items-center">
+        <img class="w-full h-56 object-cover rounded-2xl" src="https://scontent.fceb3-1.fna.fbcdn.net/v/t39.30808-6/495261489_2871703749669777_6522045126610493016_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeH_g-w0RLHevJk6nfqaTj4pEnxHZJBTAqYSfEdkkFMCppz2lmIJQ3s0EMvtQhooCb6O1IQT2q_rZbXHGSIHM9Wm&_nc_ohc=NFu2vwgFPDcQ7kNvwHRFEjQ&_nc_oc=AdmOrviEPCDPSsRtjQ5NB-zh6E6CLHKr_MBtBaR_9Ay0N0BtFK5UenG3Rh9yomxoBQA&_nc_zt=23&_nc_ht=scontent.fceb3-1.fna&_nc_gid=5d6tl63VW7VJ55wETsu5Cg&oh=00_AfOIyd3h88CVkmyXqiOtb1lvvGMY9y1o5nXEq-Ww0Vqk-Q&oe=68472B40" alt="">
+        <div class="mt-6 text-center">
+          <h3 class="text-lg font-semibold text-gray-900">Janpaul Bustillo</h3>
+          <p class="text-gray-600">Backend Developer & Database</p>
+          <div class="mt-4 flex justify-center gap-x-4">
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">X</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M6 18L18 6M6 6l12 12"/></svg>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M16 8a6 6 0 01-12 0 6 6 0 0112 0zm-6 8a8 8 0 100-16 8 8 0 000 16zm-2-7h2v6H8v-6zm1-1a1 1 0 110-2 1 1 0 010 2zm4 1h2v3.5a1.5 1.5 0 01-3 0V10h1v1.5a.5.5 0 001 0V10z"/></svg>
+            </a>
+          </div>
+        </div>
+      </li>
+      <li class="flex flex-col items-center">
+        <img class="w-full h-56 object-cover rounded-2xl" src="https://scontent.fceb3-1.fna.fbcdn.net/v/t39.30808-6/439979555_8425004204192885_23963868889279716_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeH0U-Pj3hxQAzBvEnJgxIruFNYu4MGlHV4U1i7gwaUdXvUeN7T4iLdxMiU0_Zh9qgjKDCgCiQmkRE8jFOZ0t4oj&_nc_ohc=v27J0bsB-uUQ7kNvwFo1Am2&_nc_oc=AdlK77MB2TJUtnLVPRfMHQY2yH2Q9iBeObcXQeqSvmvXnjmZWqYcSu3idPuad0CgVbs&_nc_zt=23&_nc_ht=scontent.fceb3-1.fna&_nc_gid=nQPDVOqq6q_Ac-767_ZV4A&oh=00_AfMqpYbhDNI3YHVb2DI-Lc2BbzqK_oYnAPpfEkP95wseag&oe=68473B0E" alt="">
+        <div class="mt-6 text-center">
+          <h3 class="text-lg font-semibold text-gray-900">Kerstan Zam Davide</h3>
+          <p class="text-gray-600">Backend Developer / Support</p>
+          <div class="mt-4 flex justify-center gap-x-4">
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">X</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M6 18L18 6M6 6l12 12"/></svg>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M16 8a6 6 0 01-12 0 6 6 0 0112 0zm-6 8a8 8 0 100-16 8 8 0 000 16zm-2-7h2v6H8v-6zm1-1a1 1 0 110-2 1 1 0 010 2zm4 1h2v3.5a1.5 1.5 0 01-3 0V10h1v1.5a.5.5 0 001 0V10z"/></svg>
+            </a>
+          </div>
+        </div>
+      </li>
+      <li class="flex flex-col items-center">
+        <img class="w-full h-56 object-cover rounded-2xl" src="https://scontent.fceb3-1.fna.fbcdn.net/v/t39.30808-6/432407394_1645695422631958_4371129882617179964_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGZfkfQnY1M1Fgs01fgbasTG1J3clYceBIbUndyVhx4EgqI4qUwzsmAjH33ssUXAUgc6a4UOxAyME4pYGoPhRit&_nc_ohc=3ZIJimNdEPsQ7kNvwFvuAYK&_nc_oc=Adm24XhLhdi6XdXje7oDX80AKlfefXNalvEwAeH1FDkp-_IefMKELRBYB5OSiQ5OUdU&_nc_zt=23&_nc_ht=scontent.fceb3-1.fna&_nc_gid=DENrHTPYSJU_ZM8gCie36A&oh=00_AfNa1sibCwsI6taPkRlHv998Yy6XEUR8wdIujLaIS537Mg&oe=68472D4A" alt="">
+        <div class="mt-6 text-center">
+          <h3 class="text-lg font-semibold text-gray-900">Jaspher Lawrence Siloy</h3>
+          <p class="text-gray-600">UI/UX Designer & Front End Developer</p>
+          <div class="mt-4 flex justify-center gap-x-4">
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">X</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M6 18L18 6M6 6l12 12"/></svg>
+            </a>
+            <a href="#" class="text-gray-400 hover:text-gray-500">
+              <span class="sr-only">LinkedIn</span>
+              <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M16 8a6 6 0 01-12 0 6 6 0 0112 0zm-6 8a8 8 0 100-16 8 8 0 000 16zm-2-7h2v6H8v-6zm1-1a1 1 0 110-2 1 1 0 010 2zm4 1h2v3.5a1.5 1.5 0 01-3 0V10h1v1.5a.5.5 0 001 0V10z"/></svg>
+            </a>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+
 </div>
 
 </div>
