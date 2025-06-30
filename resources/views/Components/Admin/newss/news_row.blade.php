926861c33ef33625582a07ced3d61dfc0e1e0b44
Jaspher Siloy <jaspherlawrencesiloy09@gmail.com>
2025-06-30 15:19:20 +0800

    News row
    dropdown fix (problem: dropdown menu is inside the child div which it cuts off half of the menu.)

--- resources/views/Components/Admin/newss/news_row.blade.php
+++ resources/views/Components/Admin/newss/news_row.blade.php
@@ -69,7 +69,7 @@
                 x-transition:leave="transition ease-in duration-75"
                 x-transition:leave-start="transform opacity-100 scale-100"
                 x-transition:leave-end="transform opacity-0 scale-95"
-                class="absolute right-0 mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
+                class="fixed z-[9999] right-8 top-auto mt-2 w-36 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                 role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div class="py-1" role="none">
                     {{-- Update Link --}}
