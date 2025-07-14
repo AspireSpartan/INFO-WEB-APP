48cc98acf6e5613f4f0660c1ccbb27e7112cb0ce
Janpaul Bustillo GitHub Switch <134720445+AspireSpartan@users.noreply.github.com>
2025-07-15 04:31:47 +0800

    GOVERNMENT LINKS is now working for edits
    some also were fixed but i hope they will work seamlessly.

---
+++ app/Http/Controllers/FooterTitleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FooterTitle;
+use Illuminate\Http\Request;
+
+class FooterTitleController extends Controller
+{
+    public function index()
+    {
+        $footertitle = FooterTitle::first();
+        return view('Components.Admin.Ad-Header.Ad-Header', compact('footertitle'));
+        //return response()->json($title);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'government_links_title' => 'required|string|max:255',
+        ]);
+
+        $title = FooterTitle::first();
+        if ($title) {
+            $title->update(['government_links_title' => $request->government_links_title]);
+        } else {
+            $title = FooterTitle::create(['government_links_title' => $request->government_links_title]);
+        }
+
+        return response()->json($title);
+    }
+}
